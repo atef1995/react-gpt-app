4362ef144ec8fe778f96f963057078eaf46bcb59
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2023-10-28 12:55:33 +0200

    modified:   backend/api/main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,7 +13,7 @@ origins = [
     "http://18.184.24.8",
     "http://18.156.155.238",
     "http://backendBalancer-614402920.eu-central-1.elb.amazonaws.com",
-    "http://frontendl-463996173.eu-central-1.elb.amazonaws.com"
+    "http://frontendl-463996173.eu-central-1.elb.amazonaws.com",
     # add any other origins you need to allow requests from
     "http://192.168.1.233:3000",
     "http://127.0.0.1",
